e9f51f85d56f588e640d2c35df84d1567881b2d8
Swapnil More <swapnilmore@iisc.ac.in>
2020-03-31 16:12:25 +0530

    Started React Progressive Web App

---
+++ React_Web_Design/Ch02/app.js
@@ -0,0 +1,9 @@
+(function() {
+  "use strict";
+  var ProductCustomizer = React.createElement(
+      "div",
+      {className:"customizer"},
+      "Product Customizer will go here"
+  );
+  ReactDOM.render(ProductCustomizer,document.getElementById("react-root"));
+})();
