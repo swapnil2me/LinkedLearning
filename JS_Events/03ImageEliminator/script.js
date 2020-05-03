ece6db01ca9bf6e15ff6b679c13214fe287b81a3
Swapnil More <swapnilmore@iisc.ac.in>
2020-05-03 12:39:54 +0530

    Bug fixes

--- JS_Events/03ImageEliminator/script.js
+++ JS_Events/03ImageEliminator/script.js
@@ -1,6 +1,18 @@
 document.querySelector('.grid').addEventListener('click',function(e) {
-  console.log(e);
-  var removeTarget = e.target.parentNode;
-  console.log(removeTarget.parentNode);
-  removeTarget.parentNode.removeChild(removeTarget);
+  console.dir(e.target);
+  if (e.target.tagName === 'IMG') {
+    let howMany = this.querySelectorAll('li').length;
+    console.log(howMany);
+    if (howMany > 1) {
+      let removeTarget = e.target.parentNode;
+      removeTarget.parentNode.removeChild(removeTarget);
+    }else {
+      let photoTitle = e.target.alt;
+      console.log(photoTitle);
+      document.querySelector('#art p').innerHTML = photoTitle;
+      console.dir(document.querySelector('#art p').innerHTML);
+    }
+
+  }
+
 }, false);//Bubling mode
