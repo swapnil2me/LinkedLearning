36d88e88e73913010e50acf5ec51d09dd75890b1
swapnil2me <swapnilmore@iisc.ac.in>
2020-05-25 18:36:11 +0530

    using Animation Frame instead of set and clear interval

--- JS_Animation/01_Circle/app.js
+++ JS_Animation/01_Circle/app.js
@@ -1,13 +1,29 @@
-var ol=0;
-var circleInterval = setInterval(circleAnimation,50);
+// var ol=0;
+// var circleInterval = setInterval(circleAnimation,50);
+//
+// function circleAnimation() {
+//   if (ol >= 4000/50) {
+//     document.getElementById("circle").style.backgroundColor = "dimgrey";
+//     clearInterval(circleInterval);
+//   }else {
+//     ol++;
+//     document.getElementById("circle").style.left = ol + 'px';
+//   }
+//
+// }
 
-function circleAnimation() {
-  if (ol >= 4000/50) {
-    document.getElementById("circle").style.backgroundColor = "dimgrey";
-    clearInterval(circleInterval);
+
+var ol = 0;
+var circleInterval;
+function circleAnimation(time) {
+  ol ++;
+  document.getElementById("circle").style.left = ol + "px";
+  if (time<4000) {
+    circleInterval = requestAnimationFrame(circleAnimation);
   }else {
-    ol++;
-    document.getElementById("circle").style.left = ol + 'px';
+    document.getElementById("circle").style.backgroundColor = "dimgrey";
+    cancelAnimationFrame(circleInterval);
   }
-
 }
+
+circleInterval=requestAnimationFrame(circleAnimation);
